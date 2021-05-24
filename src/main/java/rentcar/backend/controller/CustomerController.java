9b2cedb883e526db598cbaccfa2dadc99a8b04da
samet44 <sametkibriz44@gmail.com>
2021-05-24 16:32:34 +0300

    delete customer service added

--- src/main/java/rentcar/backend/controller/CustomerController.java
+++ src/main/java/rentcar/backend/controller/CustomerController.java
@@ -3,7 +3,9 @@ package rentcar.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rentcar.backend.entity.Customer;
@@ -12,7 +14,7 @@ import rentcar.backend.service.CustomerService;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("customers")
+@RequestMapping("/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -30,5 +32,10 @@ public class CustomerController {
     @PostMapping
     public Customer addCustomer(@Valid @RequestBody Customer customer){
         return customerService.addCustomer(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable int id){
+        customerService.deleteCustomer(id);
     }
 }
--- src/main/java/rentcar/backend/service/CustomerService.java
+++ src/main/java/rentcar/backend/service/CustomerService.java
@@ -3,12 +3,18 @@ package rentcar.backend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rentcar.backend.entity.Customer;
+import rentcar.backend.exception.NotFoundException;
 import rentcar.backend.repository.CustomerRepository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+
+    private Optional<Customer> optionalCustomer;
 
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
@@ -19,7 +25,21 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
+    @Transactional
     public Customer addCustomer(Customer customer){
         return customerRepository.save(customer);
+    }
+
+    @Transactional
+    public void deleteCustomer(int id){
+        if (customerIsPresent(id)){
+            throw new NotFoundException("Customer is not found");
+        }
+        customerRepository.deleteById(id);
+    }
+
+    private boolean customerIsPresent(int id) {
+        optionalCustomer = customerRepository.findById(id);
+        return optionalCustomer.isPresent();
     }
 }
